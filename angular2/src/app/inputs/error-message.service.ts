aff6b345c9cba2b092a2a2de11caf0a545b06c60
Marco Hernandez <marcohern@gmail.com>
2017-04-26 15:00:13 -0500

    missed file

--- angular2/src/app/inputs/error-message.service.ts
+++ angular2/src/app/inputs/error-message.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Injectable()
 export class ErrorMessageService {
@@ -10,6 +10,26 @@ export class ErrorMessageService {
   public message = { }
   public status  = { }
 
+  public build(controls) {
+    let group = new FormGroup({});
+    let all_messages = [];
+    for(let control_id in controls) {
+      let ctr = controls[control_id].control;
+      all_messages[control_id] = controls[control_id].messages;
+      
+      let defval = null;
+      let vals = null;
+      let asyncvals = null;
+      if(ctr.length > 0) defval = ctr[0];
+      if(ctr.length > 1) vals = ctr[1];
+      if(ctr.length > 2) asyncvals = ctr[2];
+
+      group.addControl(control_id, new FormControl(defval,vals,asyncvals));
+    }
+    this.rig(group, all_messages);
+    return group;
+  }
+
   public rig(g:FormGroup, messages) {
     this.errors = {};
     this.message = {};
@@ -17,7 +37,7 @@ export class ErrorMessageService {
 
     for (let control_id in messages) {
       let control = g.get(control_id);
-      control.statusChanges.subscribe(status => this.displayStatus(g, control_id, status));
+      control.statusChanges.subscribe(status => this.displayStatus(g, control_id, status) );
       this.message[control_id] = '';
       this.status[control_id]  = '';
     }
@@ -28,12 +48,14 @@ export class ErrorMessageService {
     for (let control_id in g.controls) {
       this.message[control_id] = '';
       let control = g.get(control_id);
-      if (control.dirty || control.valid) {
+      if (control.invalid && control.dirty) {
         for (let key in control.errors) {
           if (this.errors[control_id] && this.errors[control_id][key])
             this.message[control_id] += this.errors[control_id][key] + ' ';
           else this.message[control_id] += "["+key+"]";
         }
+      } else {
+
       }
     }
   }
