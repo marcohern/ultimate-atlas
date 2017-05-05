00a64b61179a8b048bbfc42d1036ac436b9b9b9c
Marco Hernandez <marcohern@gmail.com>
2017-05-05 17:50:01 -0500

    seting up modern form in daily trans

--- angular2/src/app/modules/daily/daily.module.ts
+++ angular2/src/app/modules/daily/daily.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { CommonModule,DatePipe } from '@angular/common'
+
+import { InputsModule } from '../inputs/inputs.module'
 import { DailyRoutes } from './daily.routes' 
 
 import { TransListComponent } from './trans-list/trans-list.component'
@@ -12,6 +14,8 @@ import { DailyService } from './daily.service'
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
+    InputsModule,
     DailyRoutes
   ],
   declarations: [TransListComponent, TransDetailComponent],
--- angular2/src/app/modules/daily/trans-detail/trans-detail.component.ts
+++ angular2/src/app/modules/daily/trans-detail/trans-detail.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Router } from '@angular/router'
 import { DatePipe } from '@angular/common'
+import { FormGroup, FormControl } from '@angular/forms'
 
 import { DailyTrans } from '../daily-trans'
 import { DailyCat } from '../daily-cat'
 import { DailyService } from '../daily.service'
+
+import { ErrorMessageService } from '../../inputs/error-message.service'
+import { UaValidators } from '../../inputs/ua-validators'
 
 @Component({
   moduleId: module.id,
@@ -14,81 +18,44 @@ import { DailyService } from '../daily.service'
 })
 export class TransDetailComponent implements OnInit {
   
-  edate:string;
-  etime:string;
-  dateReadOnly:boolean = false;
 
+  private transForm:FormGroup;
   private MINUTE:number = 1000*60;
-
-  trans:DailyTrans = {
-    event_date: new Date(),
-    cat_id: 0,
-    user_id: 0,
-    value:0.0,
-    type:'CASH',
-    category:'',
-    status:''
-  };
-
+  
   cats:DailyCat[] = [];
 
   constructor(
     private ds:DailyService,
     private datepipe: DatePipe,
     private route:ActivatedRoute,
-    private router:Router)
+    private router:Router,
+    private ems:ErrorMessageService,
+    private uav:UaValidators)
   { }
-
-  private displayDate() {
-    this.edate = this.datepipe.transform(this.trans.event_date, "yyyy-MM-dd");
-    this.etime = this.datepipe.transform(this.trans.event_date, "HH:mm:ss");
-  }
-
-  onNow() {
-    this.trans.event_date = new Date();
-    this.displayDate();
-  }
-
-  onSub(minOffset:number=0) {
-    this.trans.event_date = new Date(this.trans.event_date.valueOf() - minOffset*this.MINUTE);
-    this.displayDate();
-  }
-
-  saveTrans() {
-    console.log("TransDetailComponent.saveTrans",this.trans);
-
-    let id = +this.route.snapshot.params['id'];
-
-    this.ds.saveTransaction(this.trans, id).subscribe(data => {
-      this.router.navigate(['/daily/trans']);
-    });
-  }
 
   ngOnInit() {
     let id = +this.route.snapshot.params['id'];
+    let now = new Date();
+    let today = this.datepipe.transform(now, "yyyy-MM-dd");
+    let seconds = this.datepipe.transform(now, "HH:mm:ss");
 
-    if (id) {
-      this.dateReadOnly = true;
-      this.ds.getTransaction(id).subscribe(data => {
-        this.trans = data;
-        this.displayDate();
-      });
-    } else {
-      this.dateReadOnly = false;
-      this.displayDate();
-    }
-
-    this.ds.getCategories().subscribe(data => {
-      this.cats = data;
+    this.transForm = this.ems.build({
+      date: {
+        control: [today],
+        messages: {}
+      },
+      time: {
+        control: [seconds],
+        messages: {}
+      }
     });
   }
 
-  onDateChange() {
-    this.displayDate();
-  }
-
-  onTimeChange() {
-    this.displayDate();
+  private submitTrans(value) {
+    console.log("submitTrans",value);
+    //this.edate = this.datepipe.transform(this.trans.event_date, "yyyy-MM-dd");
+    //this.etime = this.datepipe.transform(this.trans.event_date, "HH:mm:ss");
+    //this.trans.event_date = new Date(this.trans.event_date.valueOf() - minOffset*this.MINUTE);
   }
 
 }
