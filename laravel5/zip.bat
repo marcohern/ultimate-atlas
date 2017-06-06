7dbc51cd02ba6cecb49e4be6dacd9c293c5ee31e
Marco Hernandez <marcohern@gmail.com>
2017-06-06 16:00:14 -0500

    build number working

--- laravel5/zip.bat
+++ laravel5/zip.bat
@@ -1,5 +1,10 @@
+del ..\output\aws\ua-laravel-v*.zip
 php artisan view:clear
 php artisan cache:clear
 php artisan config:cache
 del bootstrap\cache\config.php
 7z a ..\output\aws\ua-laravel-vXX.zip -r * .[^.]* -xr!.env -xr!.env.example
+set /p Build=<build_number.txt
+set /a "Build=%Build%+1"
+rename ..\output\aws\ua-laravel-vXX.zip ua-laravel-v%Build%.zip
+echo %Build% > build_number.txt
