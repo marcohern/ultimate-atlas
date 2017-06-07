a0e03714570a8fe3b52fec97ff5746e75a603d91
Marco Hernandez <marcohern@gmail.com>
2017-06-07 12:35:01 -0500

    splash page more or less finished, have not figured out what to do with the more links yet

--- laravel5/resources/views/theory/elements/footer.blade.php
+++ laravel5/resources/views/theory/elements/footer.blade.php
@@ -6,11 +6,11 @@
                 &copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.
             </div>
             <ul class="icons">
-                <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-                <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-                <li><a href="#" class="icon fa-linkedin"><span class="label">linkedIn</span></a></li>
-                <li><a href="#" class="icon fa-pinterest-p"><span class="label">Pinterest</span></a></li>
-                <li><a href="#" class="icon fa-vimeo"><span class="label">Vimeo</span></a></li>
+                <li><a href="https://www.facebook.com/marcohern" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                <li><a href="https://www.twitter.com/marquiniopinio" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="https://www.linkedin.com/in/marco-hernandez-69013436/" class="icon fa-linkedin"><span class="label">linkedIn</span></a></li>
+                <!--li><a href="#" class="icon fa-pinterest-p"><span class="label">Pinterest</span></a></li!-->
+                <li><a href="https://www.youtube.com/user/marquiniopinio" class="icon fa-youtube"><span class="label">Youtube</span></a></li>
             </ul>
         </div>
     </div>
